a6543dc1a2724bf81a3cc19531e883efdeda2254
chwastek37 <rafalchwalek22@gmail.com>
2023-04-14 19:50:46 +0200

    Dodanie wyjątku i usunięcie zbędnych części kodu

--- skrypt.py
+++ skrypt.py
@@ -16,6 +16,7 @@ class Transformation:
         
     def Npu(self,fi):
         N = self.a / np.sqrt(1 - self.e2 * np.sin(fi)**2)
+        
         return N
     
     def hirvonen(self,X,Y,Z):
@@ -23,7 +24,6 @@ class Transformation:
         for X, Y, Z in zip(X, Y, Z):
             p = np.sqrt(X**2+Y**2)
             fi = np.arctan(Z/(p*(1-self.e2)))
-                
             while True:
                 N = self.Npu(fi)
                 h = (p/np.cos(fi)) - N
@@ -31,7 +31,6 @@ class Transformation:
                 fi = np.arctan((Z/p)/(1-((N*self.e2)/(N+h))))
                 if abs(fi_poprzednia-fi)<(0.000001/206265):
                     break
-                
             N = self.Npu(fi)
             h = p/np.cos(fi) - N
             lam = np.arctan(Y/X)
@@ -47,33 +46,28 @@ class Transformation:
             Yk = (N+h)*np.cos(fi)*np.sin(lam)
             Zk = (N*(1-self.e2)+h)*np.sin(fi)   
             wyniki.append([Xk,Yk,Zk])
+            
         return wyniki
     
     
     def pl1992(self,fi,lama,m=0.9993):  
         lama0 = np.deg2rad(19)
         wyniki = []
-        for fi, lama in zip (fi,lama) :
-        # 1 parametry elipsoidy     
+        for fi, lama in zip (fi,lama):   
             b2 = self.a**2*(1-self.e2)
             ep2 = (self.a**2-b2)/b2
-        # 2. Wielkosci pomocnicze     
             dellama = lama - lama0
             t = np.tan(fi)
             ni2 = ep2*(np.cos(fi)**2)
             N = self.Npu(fi)
-        
-        # 3. Długosc luku poludnika 
+         
             A0 = 1- (self.e2/4)-(3*self.e2**2/64)-(5*self.e2**3/256)
             A2 = (3/8)*(self.e2+(self.e2**2/4)+(15*self.e2**3/128))
             A4 = (15/256)*(self.e2**2+((3*self.e2**3)/4))
             A6 = (35*self.e2**3)/3072
             sigma = self.a *(A0*fi-A2*np.sin(2*fi)+A4*np.sin(4*fi)-A6*np.sin(6*fi))
         
-        #  wsolrzedne prostokatne lokalne na plaszczyznie gaussa-krugera
-        
             xgk =  sigma    +    ( ((dellama**2/2)*N*np.sin(fi)*np.cos(fi))    *    (1   +   ((dellama**2/12)*(np.cos(fi)**2)*(5 - t**2 + 9*ni2 + 4*ni2**2))      +         ((dellama**4/360)*(np.cos(fi)**4)*(61 - 58*t**2 + t**4 + 270*ni2 - 330*ni2*t**2))))
-        
             ygk =  (dellama* N * np.cos(fi))  *   ( 1 +  ((dellama**2/6)   *   (np.cos(fi)**2)   *  (1 - t**2 + ni2))     +     (((dellama**4/120)*(np.cos(fi)**4)) * (5 - (18*t**2) + t**4 + (14 * ni2) - (58*ni2*t**2))))
         
             x92 = xgk * m - 5300000
@@ -84,7 +78,7 @@ class Transformation:
 
     def pl2000(self,fi,lama,m=0.999923):
         wyniki = []
-        for fi, lama in zip (fi,lama) :
+        for fi, lama in zip (fi,lama):
             lama0 = 0
             strefa = 0
             if lama >np.deg2rad(13.5) and lama < np.deg2rad(16.5):
@@ -99,19 +93,14 @@ class Transformation:
             elif lama >np.deg2rad(22.5) and lama < np.deg2rad(25.5):
                 strefa = 8
                 lama0 = np.deg2rad(24)
-            # else:
-            #     print("Punkt poza strefami odwzorowawczymi układu PL-2000")        
             
-            # 1 parametry elipsoidy     
             b2 = self.a**2*(1-self.e2)    
             ep2 = (self.a**2-b2)/b2
-            # 2. Wielkosci pomocnicze     
             dellama = lama - lama0
             t = np.tan(fi)
             ni2 = ep2*(np.cos(fi)**2)
             N = self.Npu(fi)
-            
-            # 3. Długosc luku poludnika 
+             
             A0 = 1- (self.e2/4)-(3*self.e2**2/64)-(5*self.e2**3/256)
             A2 = (3/8)*(self.e2+(self.e2**2/4)+(15*self.e2**3/128))
             A4 = (15/256)*(self.e2**2+((3*self.e2**3)/4))
@@ -119,25 +108,24 @@ class Transformation:
             
             sigma = self.a *(A0*fi-A2*np.sin(2*fi)+A4*np.sin(4*fi)-A6*np.sin(6*fi))
             
-            # wsolrzedne prostokatne lokalne na plaszczyznie gaussa-krugera
-            
             xgk =  sigma    +    ( ((dellama**2/2)*N*np.sin(fi)*np.cos(fi))    *    (1   +   ((dellama**2/12)*(np.cos(fi)**2)*(5 - t**2 + 9*ni2 + 4*ni2**2))      +         ((dellama**4/360)*(np.cos(fi)**4)*(61 - 58*t**2 + t**4 + 270*ni2 - 330*ni2*t**2))))
             ygk =  (dellama* N * np.cos(fi))  *   ( 1 +  ((dellama**2/6)   *   (np.cos(fi)**2)   *  (1 - t**2 + ni2))     +     (((dellama**4/120)*(np.cos(fi)**4)) * (5 - (18*t**2) + t**4 + (14 * ni2) - (58*ni2*t**2))))
             
             x2000 = xgk * m 
             y2000 = ygk*m + (strefa *1000000) +500000
             wyniki.append([x2000,y2000])
+            
         return  wyniki
     
     def Rneu(self, phi, lam):
         Rneu = np.array([[-np.sin(phi)*np.cos(lam), -np.sin(lam), np.cos(phi)*np.cos(lam)],
                          [-np.sin(phi)*np.sin(lam), np.cos(lam), np.cos(phi)*np.sin(lam)],
                          [np.cos(phi), 0, np.sin(phi)]])
+        
         return Rneu
     
     def xyz2neup(self, X, Y, Z, X0, Y0, Z0):
         wyniki = []
-        # jak korzystalem z definicji hirvonen to blad wyskakiwal
         p = np.sqrt(X0**2+Y0**2)
         fi = np.arctan(Z0/(p*(1-self.e2)))
         while True:
@@ -156,33 +144,32 @@ class Transformation:
             X_sr = [X-X0, Y-Y0, Z-Z0] 
             X_rneu = R_neu.T@X_sr
             wyniki.append(X_rneu.T)
+            
         return wyniki
     
     def odczyt(self,plik_wsadowy, transformacja):
         dane = np.genfromtxt(plik_wsadowy,delimiter = " ")
         if transformacja == 'XYZ2BLH':
             wyniki = self.hirvonen(dane[:,0], dane[:,1], dane[:,2])
-            plik_wynikowy = np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.10f %0.10f %0.3f')
+            np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.10f %0.10f %0.3f')
         
         elif transformacja == 'BLH2XYZ':
             wyniki = self.hirvonen_odw(np.deg2rad((dane[:,0])), np.deg2rad(dane[:,1]), dane[:,2])
-            plik_wynikowy = np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f %0.3f')
+            np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f %0.3f')
 
         elif transformacja == 'PL2000':
             wyniki = self.pl2000(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
-            plik_wynikowy = np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f')
+            np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f')
         
         elif transformacja == 'PL1992':
             wyniki = self.pl1992(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
-            plik_wynikowy = np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f')
+            np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f')
         
         elif transformacja == 'XYZ2NEUP':
             wyniki = self.xyz2neup(dane[1:,0], dane[1:,1], dane[1:,2], dane[0,0], dane[0,1], dane[0,2])
-            plik_wynikowy = np.savetxt(f"plik_wynikowy_{transformacja}._{args.el}txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f %0.3f')
-        return
+            np.savetxt(f"plik_wynikowy_{transformacja}._{args.el}txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f %0.3f')
     
 if __name__ == '__main__':
-    # try:
     parser = ArgumentParser()
     parser.add_argument('-p', type=str, help='Przyjmuje sciezke do pliku z danymi wejsciowymi, jesli plik jest w tym samym folderze co skrypt to wystarczy nazwa pliku z rozszerzeniem')
     parser.add_argument('-el', type=str, help='Przyjmuje nazwe elipsoidy. Dostepne: WGS84, GRS80 lub KRASOWSKI')
@@ -190,52 +177,35 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     elipsoidy = {'WGS84':[6378137.000, 0.00669438002290], 'GRS80':[6378137.000, 0.00669438002290], 'KRASOWSKI':[6378245.000, 0.00669342162296]}
-        
-        # te ify sa po to zeby dzialalo jesli nie odpalasz przez cmd albo nie uzywasz flag
-        # jesli nie chcesz co chwila wpisywac danych to to odkomentuj i zmieniaj na co chcesz
-        # args.el = 'wgs84'
-        # args.p = 'plik_dane_fi_lam.txt'
-        # args.t = 'pl1992'
+    transformacje = {'XYZ2BLH': 'XYZ2BLH','BLH2XYZ': 'BLH2XYZ','PL2000':'PL2000','PL1992':'PL1992', 'XYZ2NEUP':'XYZ2NEUP'}
+    
+    wybor = "TAK"
     try:
-        if args.el==None:
-            args.el = input(str('Podaj nazwe elipsoidy: '))
-            
-        obiekt = Transformation(elipsoidy[args.el.upper()])
-    except KeyError:
-        print('Zle podano nazwe elipsoidy')
-        print('Koniec programu')
-    else:
-        wybor = "TAK"
-        try:
-            while wybor =="TAK":
+        while wybor =="TAK":
+            if args.el==None:
+                args.el = input(str('Podaj nazwe elipsoidy: '))
+            if args.p==None:
+                args.p = input(str('Wklej sciezke do pliku txt z danymi: '))
+            if args.t==None:
+                args.t = input(str('Jaka transformacje wykonac?: '))
+                    
+            obiekt = Transformation(elipsoidy[args.el.upper()])
+            dane = obiekt.odczyt(args.p, transformacje[args.t.upper()])
                 
-                transformacje = {'XYZ2BLH': 'XYZ2BLH','BLH2XYZ': 'BLH2XYZ','PL2000':'PL2000','PL1992':'PL1992', 'XYZ2NEUP':'XYZ2NEUP'}
-                if args.el==None:
-                    args.el = input(str('Podaj nazwe elipsoidy: '))
-                if args.p==None:
-                    args.p = input(str('Wklej sciezke do pliku txt z danymi: '))
-        
-                if args.t==None:
-                    args.t = input(str('Jaka transformacje wykonac?: '))
-                obiekt = Transformation(elipsoidy[args.el.upper()])
-
-                dane = obiekt.odczyt(args.p, transformacje[args.t.upper()])
-                wybor = input(str("Jezeli chcesz wykonać kolejną transformację wpisz TAK jesli chcesz zakończyć NIE:")).upper()
-                args.el = None
-                args.p=None
-                args.t=None
-
-
-        except FileNotFoundError:
-            print('Podany plik nie istnieje.')
-        except KeyError:
-            print('Zle podana transformacja.')
-        except IndexError:
-            print('Zly format danych w pliku.')
-       
-        else:
             print('Plik wynikowy zostal utworzony.')
-        finally:
-            print('Koniec programu')
-# if wybor != "TAK":
-    #     z =3
+                
+            wybor = input(str("Jezeli chcesz wykonac kolejna transformacje wpisz TAK jesli chcesz zakonczyc NIE: ")).upper()
+            args.el = None
+            args.p= None
+            args.t= None
+
+    except FileNotFoundError:
+        print('Podany plik nie istnieje.')
+    except KeyError:
+        print('Zle podana elipsoida lub transformacja.')
+    except IndexError:
+        print('Zly format danych w pliku.')
+    except ValueError:
+        print('Zly format danych w pliku.')
+    finally:
+        print('Koniec programu')
